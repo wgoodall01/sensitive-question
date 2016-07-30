c09f07c8efa7d882e6a0d0bd62df5940e6e77f6a
William Goodall <wgoodall01@gmail.com>
2016-07-30 14:45:39 -0400

    Mostly finished frontend

--- static/main.js
+++ static/main.js
@@ -1,6 +1,25 @@
 angular.module("questions", ["ngMaterial", "ngMessages", "ngMdIcons"])
 
-.controller("qCtrl", function($scope){
-    $scope.test = "ayy lmao";
+.controller("qCtrl", function($scope, $http){
+    $scope.tabIndex = 0;
+
+    $scope.next = () => { $scope.tabIndex++; };
+    $scope.reset = () => {
+        $scope.tabIndex = 0;
+        $scope.submissionState = "none";
+    };
+
+    $scope.newRandom = () => { $scope.lie = Math.random() < 0.25; };
+
+    $scope.submissionState = "none";
+    $scope.submit = function(answer){
+        $scope.submissionState = "waiting";
+        $http.post("/answer", {answer:answer})
+            .then(function(){
+                $scope.submissionState = "done";
+            }).catch(function(){
+                $scope.submissionState = "error";
+            });
+    };
 });
 
